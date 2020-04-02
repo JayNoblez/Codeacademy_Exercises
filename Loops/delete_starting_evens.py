39bec678b1b7147d1c6bc73a3c8f6999874451bc
John Adedigba <john.adedigba@gmail.com>
2020-04-02 20:15:39 +0200

    Create delete_starting_evens.py

---
+++ Loops/delete_starting_evens.py
@@ -0,0 +1,24 @@
+#Write a function called delete_starting_evens() that has a parameter named lst.
+
+#The function should remove elements from the front of lst until the front of the list is not even. The function should then return lst.
+
+#For example if lst started as [4, 8, 10, 11, 12, 15], then delete_starting_evens(lst) should return [11, 12, 15].
+
+#Make sure your function works even if every element in the list is even!
+
+# Use a while loop to check two things. First, check if the list has at least one element, using len(lst). 
+#Second, check to see if the first element is odd using mod (%). 
+#If both of those are True, slice off the first element of the list using lst = lst[1:].
+
+def delete_starting_evens(lst):
+  while len(lst) > 0: 
+    if lst[0] % 2 == 0:
+      lst = lst[1:]
+    else: break
+  return lst
+  
+  
+
+#Uncomment the lines below when your function is done
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
